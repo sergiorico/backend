c7862b55f93adaa97f848486db854be7ab29e223
Axel Smeets <dat12asm@student.lu.se>
2016-10-10 10:29:37 +0200

    Update registration test to also test /verify

--- src/test/java/se/lth/cs/connect/TestUserAPI.java
+++ src/test/java/se/lth/cs/connect/TestUserAPI.java
@@ -1,17 +1,26 @@
 package se.lth.cs.connect;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 import org.junit.Rule;
 import org.junit.Test;
 
+import com.jayway.restassured.filter.session.SessionFilter;
 import com.jayway.restassured.response.Response;
 
+import ro.pippo.core.PippoConstants;
 import ro.pippo.test.PippoRule;
 import ro.pippo.test.PippoTest;
+import utils.URLParser;
 
 public class TestUserAPI extends PippoTest {
-
+	
 	// This rule ensures that we have a server running before doing the tests
 	public Connect app = new Connect();
 	@Rule
@@ -32,36 +41,55 @@ public class TestUserAPI extends PippoTest {
 		when().get("/v1/account/dat12asm@student.lu.se").then().statusCode(401);
 	}
 	
+	
 	/**
 	 * Verify that 1) an email is sent after signup, and 2) that the email
 	 * is sent to the correct email address.
 	 */
 	@Test
-	public void testRegistration() {
+	public void testRegistration() throws UnsupportedEncodingException {
 		// Use mailbox to capture emails instead of sending them
 		Mailbox mailbox = new Mailbox();
 		app.useMailClient(mailbox);
 		
+		SessionFilter sessionFilter = new SessionFilter();
+		
 		// Register
-		Response reg = given().
-				param("email", "test-reg@serptest.test").
-				param("passw", "hejsanhoppsan").
+		given().
+			param("email", "test-reg@serptest.test").
+			param("passw", "hejsanhoppsan").
+			filter(sessionFilter).
+		expect().
+			statusCode(200).
+		when().
 			post("/v1/account/register");
 		
-		reg.then().
-			statusCode(200);
 	
-		assertEquals("Must send registration email", mailbox.getInbox().size(), 1);
-		assertEquals("Recipient must match registered email",
-				mailbox.top().recipient, "test-reg@serptest.test");
+		assertThat("Must send registration email", mailbox.getInbox().size(), is(1));
+		assertThat("Recipient must match registered email",
+				mailbox.top().recipient, is(equalTo("test-reg@serptest.test")));
+		
+		String verify = URLParser.find(mailbox.top().content);
+		verify = verify.substring(verify.indexOf("token=") + 6);
+		assertThat("Must find link in email", verify, not(equalTo("")));
+		
+		verify = URLDecoder.decode(verify, PippoConstants.UTF8);
+
+		given().
+			//filter(sessionFilter).
+			param("token", verify).
+		expect().
+			statusCode(200).
+		when().
+			get("/v1/account/verify");
 		
 		// Cleanup
 		given().
-			cookie("JSESSIONID", reg.getCookie("JSESSIONID")).
+			filter(sessionFilter).
+		expect().
+			statusCode(200).
 		when().
-			post("/v1/account/delete").
-		then().
-			statusCode(200);
+			post("/v1/account/delete");
 	}
 	
 	@Test
