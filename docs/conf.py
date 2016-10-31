3e86ca9e3764a0e5e7ba18a465b2f7154a66b9f9
Axel Smeets <dat12asm@student.lu.se>
2016-10-31 15:55:12 +0100

    Update config to maybe fix master file

--- docs/conf.py
+++ docs/conf.py
@@ -7,3 +7,11 @@ sys.path.insert(0, os.path.abspath('..'))
 
 sys.path.append(os.path.abspath('_ext'))
 extensions = ['sphinxcontrib.httpdomain']
+
+master_doc = 'index'
+
+project = 'SERP Connect'
+copyright = '2015-2016, ??'
+author = '??'
+
+source_suffix = [ '.rst', '.md' ]
