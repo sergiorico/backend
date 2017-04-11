8a2fc88e96a945f4dcfbe549ff9145a9006ee104
Axel Smeets <dat12asm@student.lu.se>
2017-04-11 19:46:30 +0200

    Refactor integration tests

---
+++ src/test/java/se/lth/cs/connect/APITest.java
@@ -0,0 +1,125 @@
+package se.lth.cs.connect;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.Random;
+
+import org.junit.After;
+
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Rule;
+
+import com.jayway.restassured.filter.session.SessionFilter;
+
+import iot.jcypher.database.IDBAccess;
+import ro.pippo.core.PippoConstants;
+import ro.pippo.test.PippoRule;
+import ro.pippo.test.PippoTest;
+import se.lth.cs.connect.modules.Database;
+import se.lth.cs.connect.modules.MailClient;
+import utils.URLParser;
+
+public class APITest extends PippoTest {
+	public SessionFilter userSession = new SessionFilter();
+	public String email = "";
+	public String passw = "";
+
+    public String collection = "x";
+    public long collectionId = -1;
+	
+    // This rule ensures that we have a server running before doing the tests
+	public Connect app = new Connect();
+
+	@Rule
+	public PippoRule pippoRule = new PippoRule(app);
+
+    public SessionFilter registerUser(SessionFilter sf, String email, String passw) {
+        given().
+            filter(sf).
+            param("email", email).
+            param("passw", passw).
+        when().
+            post("/v1/account/register").
+        then().
+            statusCode(200);
+
+        return sf;
+    }
+    public SessionFilter registerUser(String email, String passw) {
+        return registerUser(new SessionFilter(), email, passw);
+    }
+
+	public String verifyUser(Mailbox mailbox) throws UnsupportedEncodingException{
+		String verify = URLParser.find(mailbox.top().content);
+		verify = verify.substring(verify.indexOf("token=") + 6);
+		verify = URLDecoder.decode(verify, PippoConstants.UTF8);
+		
+        given().
+			param("token", verify).
+		expect().
+			statusCode(200).
+		when().
+			get("/v1/account/verify");
+		
+		return verify;
+	}
+	
+	public String getRandomString() {
+        final String CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < 10) {
+            int index = (int) (rnd.nextFloat() * CHARS.length());
+            salt.append(CHARS.charAt(index));
+        }
+        String saltStr = salt.toString();
+        return saltStr;
+
+    }
+
+    public void setupUser(SessionFilter sf, String email, String passw) {
+        MailClient client = app.getMailClient();
+		Mailbox mailbox = new Mailbox();
+		app.useMailClient(mailbox);
+		registerUser(sf, email, passw);
+        try {
+			verifyUser(mailbox);
+		} catch (UnsupportedEncodingException uee) {
+			org.junit.Assert.fail("Failed to verify user during setUp");
+		}
+		app.useMailClient(client);
+    }
+	
+    public long setupCollection(SessionFilter sf, String name) {
+        final String id = given().
+            filter(sf).
+            param("name", name).
+        expect().
+            statusCode(200).
+        when().
+            post("/v1/collection/").
+        andReturn().
+            jsonPath().getString("id");
+        return Long.parseLong(id);
+    }
+
+	@Before
+	public void setUp() {
+        email = getRandomString() + "@" + getRandomString() + ".com";
+		passw = getRandomString();
+		setupUser(userSession, email, passw);
+		
+        collection = getRandomString();
+        collectionId = setupCollection(userSession, collection);
+	}
+	
+	@AfterClass
+	public static void afterClass() {
+		IDBAccess access = Database.access();
+		access.clearDatabase();
+		access.close();
+	}
+
+}
--- src/test/java/se/lth/cs/connect/ITUserAPI.java
+++ src/test/java/se/lth/cs/connect/ITUserAPI.java
@@ -5,34 +5,20 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
 
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.util.Random;
 
-import org.junit.Rule;
+import org.junit.Before;
 import org.junit.Test;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.restassured.filter.session.SessionFilter;
-import com.jayway.restassured.internal.mapping.Jackson1Mapper;
-import com.jayway.restassured.path.json.JsonPath;
-import com.jayway.restassured.response.Response;
 
 import ro.pippo.core.PippoConstants;
-import ro.pippo.test.PippoRule;
-import ro.pippo.test.PippoTest;
 import se.lth.cs.connect.Mailbox.Mail;
-import utils.URLParser;
+import se.lth.cs.connect.modules.MailClient;
 
-public class ITUserAPI extends PippoTest {
-
-	// This rule ensures that we have a server running before doing the tests
-	public Connect app = new Connect();
-	@Rule
-	public PippoRule pippoRule = new PippoRule(app);
+public class ITUserAPI extends APITest {
 
 	@Test
 	public void testAccessDenied() {
@@ -49,41 +35,17 @@ public class ITUserAPI extends PippoTest {
 		when().get("/v1/account/dat12asm@student.lu.se").then().statusCode(401);
 	}
 
-	private void register(String email, String passw, SessionFilter creds) {
-		Response reg = given().
-				filter(creds).
-				param("email", email).
-				param("passw", passw).
-			post("/v1/account/register");
-
-		reg.then().statusCode(200);
-	}
-
-	private void delete(SessionFilter creds) {
-		given().
-			filter(creds).
-		when().
-			post("/v1/account/delete").
-		then().
-			statusCode(200);
-	}
-
 	/**
 	 * Verify that the /login route returns 200 when logged in.
 	 */
 	@Test
 	public void testLogin() {
-		SessionFilter session = new SessionFilter();
-		register("test-login@serp", "1234", session);
-
 		given().
-			filter(session).
+			filter(userSession).
 		when().
 			get("/v1/account/login").
 		then().
 			statusCode(200);
-
-		delete(session);
 	}
 
 	/**
@@ -96,178 +58,70 @@ public class ITUserAPI extends PippoTest {
 		Mailbox mailbox = new Mailbox();
 		app.useMailClient(mailbox);
 		
-		SessionFilter session = new SessionFilter();
-		
-		register("test-reg@serptest.test", "hejsanhoppsan", session);
+		registerUser("test-reg@a.b", "1234");
 	
 		assertThat("Must send registration email", mailbox.getInbox().size(), is(1));
 		assertThat("Recipient must match registered email",
-				mailbox.top().recipient, is(equalTo("test-reg@serptest.test")));
+				mailbox.top().recipient, is(equalTo("test-reg@a.b")));
 		
 		String verify = verifyUser(mailbox);
-		assertThat("Must find link in email", verify, not(equalTo("")));		
-		
-		// Cleanup
-		delete(session);
-	}
-	
-	/**
-	 * Tests that the collection check filter does not accept a string an id and that the id exist in the database.
-	 */
-	@Test
-	public void testCollectionIdCheckExistanceFilter(){
-		
-		expect().
-			statusCode(400).
-		when().
-			get("v1/collection/" + 19453324 + "/stats"); 
-		
-		expect().
-			statusCode(400).
-		when().
-			get("v1/collection/" + "hej" + "/stats");
-	}
-	
-	/**
-	 * Tests that when the last person of a collections leaves the collection, the collection is deleted.
-	 * Also tests that entries that are part of that collection are also deleted.
-	 * @throws JsonParseException 
-	 * @throws UnsupportedEncodingException 
-	 */
-	@Test
-	public void testDeleteEmptyCollections() throws JsonParseException, UnsupportedEncodingException {
-		Mailbox mailbox = new Mailbox();
-		app.useMailClient(mailbox);
-		
-		SessionFilter session = new SessionFilter();
-		register("Filippa@serp.test", "hej", session);	
-		
-		verifyUser(mailbox);
-		
-		Response res = given().
-			filter(session).param("name", "testcoll").
-		expect().
-			statusCode(200).
-			contentType("application/json").
-		when().
-			post("v1/collection/");
-		
-		JsonPath jp = res.jsonPath();
-		String id = jp.getString("id");
-		
-		//Add entry. json String can be whatever as long as it is a valid json String for an entry.
-		String json = "{ \"entryType\": \"challenge\", \"description\": \"test\", \"serpClassification\": {}, \"collection\": " + id + " }";
-		res = given().
-			contentType("application/json").
-			filter(session).body(json).
-		expect().
-			statusCode(200).
-			contentType("application/json").
-		when().
-			post("v1/entry/new");
-		
-		jp = res.jsonPath();
-		String entryID = jp.getString("id");
-		
-		given().
-			filter(session).
-		expect().
-			statusCode(200).
-		when().
-			post("v1/collection/" + id + "/leave");
-		
-		expect().
-			statusCode(400).
-		when().
-			get("v1/collection/" + id + "/stats");
-		
-		//Test that entry was also automatically deleted.
-		expect().
-			statusCode(400).
-		when().
-			get("v1/entry/" + entryID);
-		
-		delete(session);
-	}
-	
-	/**
-	 * Tests that a new collection is not created when the collection id is a string when making a new entry.
-	 * @throws UnsupportedEncodingException 
-	 */
-	@Test
-	public void testNoCollectionCreationOnNewEntryWhenCollectionIdIsString() throws UnsupportedEncodingException{
-		Mailbox mailbox = new Mailbox();
-		app.useMailClient(mailbox);
-		
-		SessionFilter session = new SessionFilter();
-		register("Filippaaaaa@serp.test", "hejaaaaa", session);
-		
-		verifyUser(mailbox);
+		assertThat("Must find link in email", verify, not(equalTo("")));
 
-		//Create entry. collection has a string value instead of an integer to test that it doesn't create a new collection.
-		String json = "{ \"entryType\": \"challenge\", \"description\": \"test\", \"serpClassification\": {}, \"collection\": \"hejj\" }";
-		given().
-			contentType("application/json").
-			filter(session).body(json).
 		expect().
 			statusCode(400).
+		given().
+			param("email", "test-reg@a.b").
+			param("passw", "1").
 		when().
-			post("v1/entry/new");
-		
-		delete(session);	
+			post("/v1/account/register");
 	}
 
 	@Test
 	public void testLifecycle() {
-		// This test case is not very important, but it showcases how to
-		// (re)use a session for multiple queries and how to create post
-		// requests with data.
-		SessionFilter session = new SessionFilter();
-		register("test@serptest.test", "hejsanhoppsan", session);
-
 		given().
-			filter(session).
+			filter(userSession).
 		expect().
 			statusCode(200).
 		when().
 			post("/v1/account/logout");
 
 		given().
-			filter(session).
-			param("email", "test@serptest.test").
-			param("passw", "hejsanhoppsan").
+			filter(userSession).
+			param("email", email).
+			param("passw", passw).
 		expect().
 			statusCode(200).
 		when().
 			post("/v1/account/login");
 
-		delete(session);
+		given().
+			filter(userSession).
+		when().
+			post("/v1/account/delete").
+		then().
+			statusCode(200);
 	}
 	
-	//this testcase might break if format of the reset password email is changed
+	// This testcase might break if format of the reset password email is changed
 	@Test
 	public void testResetPassword() throws UnsupportedEncodingException{
 		Mailbox mailbox = new Mailbox();
 		app.useMailClient(mailbox);
-		
-		SessionFilter session = new SessionFilter();
-		
-		// Register
-		register("test-reset@serptest.test", "hejsanhoppsan",session);
-		
-		
+				
 		//logout
 		given().
-			filter(session).
+			filter(userSession).
 		when().
 			post("/v1/account/logout").
 		then().
 			statusCode(200);
 		
 		//ask for reset email
-		Response reset = given().
-				param("email", "test-reset@serptest.test").
-				expect().statusCode(200).when().
+		given().
+			param("email", email).
+		expect().
+			statusCode(200).
+		when().
 			post("/v1/account/reset-password");
 		
 		//filter mail for the token
@@ -278,47 +132,36 @@ public class ITUserAPI extends PippoTest {
 
 		//verify token
 		//stop redirect to avoid breaking the session
-		String sessionId = given().redirects().follow(false).
+		String sessionId = given().
+				redirects().follow(false).
 				param("token", verify).
-				expect().statusCode(302).when().
-				get("/v1/account/reset-password").andReturn().sessionId();
-		
-		//System.err.println("after: " + sessionId);
+			expect().
+				statusCode(302).
+			when().
+				get("/v1/account/reset-password").
+			andReturn().
+				sessionId();
 
 		//set new password
 		given().
 			cookie("JSESSIONID", sessionId).
-			param("passw","hej123").
-		expect().statusCode(200).when().
+			param("passw", "hej123").
+		expect().
+			statusCode(200).
+		when().
 			post("/v1/account/reset-password-confirm");
 	
 		//session gets wrecked by redirect no need to logout
 
 		//login with new password
 		given().
-		filter(session).
-		param("email", "test-reset@serptest.test").
-		param("passw", "hej123").
+			filter(userSession).
+			param("email", email).
+			param("passw", "hej123").
 		when().
-		post("/v1/account/login").
+			post("/v1/account/login").
 		then().
-		statusCode(200);
-
-		delete(session);
-	}
-	
-	private String verifyUser(Mailbox mailbox) throws UnsupportedEncodingException{
-		String verify = URLParser.find(mailbox.top().content);
-		verify = verify.substring(verify.indexOf("token=") + 6);
-		verify = URLDecoder.decode(verify, PippoConstants.UTF8);
-		given().
-			param("token", verify).
-		expect().
-			statusCode(200).
-		when().
-			get("/v1/account/verify");
-		
-		return verify;
+			statusCode(200);
 	}
 	
 	
