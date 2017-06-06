8108840a2fc3c863582b1a13d0964be0e89756bf
Axel Smeets <dat12asm@student.lu.se>
2017-06-06 17:07:58 +0200

    Maybe fix DEE

--- src/main/java/se/lth/cs/connect/events/DeleteEntryEvent.java
+++ src/main/java/se/lth/cs/connect/events/DeleteEntryEvent.java
@@ -6,6 +6,7 @@ import iot.jcypher.query.factories.clause.MATCH;
 import iot.jcypher.query.factories.clause.OPTIONAL_MATCH;
 import iot.jcypher.query.factories.clause.WHERE;
 import iot.jcypher.query.factories.clause.WITH;
+import iot.jcypher.query.factories.xpression.X;
 import iot.jcypher.query.values.JcNode;
 import iot.jcypher.query.values.JcRelation;
 import se.lth.cs.connect.modules.Database;
@@ -26,13 +27,16 @@ public class DeleteEntryEvent implements UserEvent {
         JcRelation rel = new JcRelation("m");
 
         Database.query(Database.access(), new IClause[]{
-            OPTIONAL_MATCH.node(entry).relation().node(facet).label("facet"),
+            OPTIONAL_MATCH.node(entry).relation(rel).node(facet).label("facet"),
             WHERE.valueOf(entry.id()).EQUALS(id),
             DO.DETACH_DELETE(entry),
-            WITH.value(facet),
-            OPTIONAL_MATCH.node(facet).relation(rel).node().label("entry"),
-            WHERE.valueOf(rel).IS_NULL(),
-            DO.DELETE(facet)
+            DO.DELETE(rel)
+        });
+
+        Database.query(Database.access(), new IClause[]{
+            MATCH.node(facet).label("facet"),
+            WHERE.NOT().existsPattern(X.node(facet).relation().node().label("entry")),
+            DO.DETACH_DELETE(facet),
         });
     }
 
